cab5bdb96911324618d47c8fe4c2693727705714
jhelindoca <jhemilyluana22@gmail.com>
2026-05-06 11:49:11 -0300

    cadastro

--- Centro_Pokemon/Centro_Pokemon/Program.cs
+++ Centro_Pokemon/Centro_Pokemon/Program.cs
@@ -14,7 +14,7 @@ namespace Centro_Pokemon
 
             int opcao = 0;
 
-          while (opcao != 4)   //diferente
+            while (opcao != 4)   //diferente
             {
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Blue; //cor do texto
@@ -37,7 +37,11 @@ namespace Centro_Pokemon
                 Console.WriteLine("\n 3 - Apagar Pokemon");
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("\n 4 - Sair");
+
+
                 Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("\n digite a opção escolhida:  ");
+                Console.ResetColor();
                 opcao = int.Parse(Console.ReadLine());
 
 
@@ -45,8 +49,11 @@ namespace Centro_Pokemon
                 switch (opcao)
                 {
                     case 1:
+                        cadastroPokemon(); //abre a função
                         break;
+                        {
 
+                        }
                     case 2:
                         break;
 
@@ -66,18 +73,77 @@ namespace Centro_Pokemon
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Opcao Inválida!!!");
                         Console.ResetColor();
-                        System.Threading.Thread.Sleep(2000);
-                        Thread.Sleep(2000);
+                        Thread.Sleep(2000); //espera por 2 segundos
                         break;
                 }
 
             }
+        }
 
+        static void cadastroPokemon()
+        {
+            Console.Clear(); // limpa a tela
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@" 
+░█████╗░░█████╗░██████╗░░█████╗░░██████╗████████╗██████╗░░█████╗░
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗
+██║░░╚═╝███████║██║░░██║███████║╚█████╗░░░░██║░░░██████╔╝██║░░██║
+██║░░██╗██╔══██║██║░░██║██╔══██║░╚═══██╗░░░██║░░░██╔══██╗██║░░██║
+╚█████╔╝██║░░██║██████╔╝██║░░██║██████╔╝░░░██║░░░██║░░██║╚█████╔╝
+░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░");
 
+            Console.ForegroundColor = ConsoleColor.Green;
+                
+            Console.WriteLine("\n digite o nome do seu Pokemon:  ");
+            string nomePokemon = Console.ReadLine();         // string = textos
 
+            Console.WriteLine("\n digite o tipo do seu Pokemon:  ");
+            string tipoPokemon = Console.ReadLine();        // string = textos
+
+            Console.WriteLine("\n digite o tamanho do seu Pokemon:  ");
+            double alturaPokemon = double.Parse(Console.ReadLine());   //double = casa decimal
+
+            Console.WriteLine("\n digite o peso do seu Pokemon:  ");
+            double pesoPokemon = double.Parse(Console.ReadLine());    //double  casa decimal
+
+            //fraqueza
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("quantas fraquezas ele tem?  :");
+            int qtdfraqueza = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= qtdfraqueza; i++)
+            {
+                Console.WriteLine($"Fraqueza  {i}  ;  ");  //{i} --- colocar a variavel no texto
+                string fraquezaPokemon = Console.ReadLine();
+
+            }
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("quantas evoluções ele tem?  :");
+                int qtdevolucao = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 1;  i <= qtdevolucao; i++)
+            {
+                Console.WriteLine($"nome da evolução  {i}  ;  ");  //{i} --- colocar a variavel no texto
+                string evolucaoPokemon = Console.ReadLine();
+
+                Console.ForegroundColor= ConsoleColor.Green ;
+                Console.WriteLine("PARABÉNS... Pokemon cadastrado");
+                Thread.Sleep(3000);
+                Console.ResetColor();
 
 
 
             }
+
+
         }
+
+
+
+
+
+
     }
+}
